c0a675be7ef5afe51aa0560593dbefe4ccf844af
Myroslav Androshchuk <support@webmiracle.com.ua>
2013-09-17 21:19:45 +0300

    MENU - 2nd type. Fixed bug in Chrome

--- stoptransit/skin/frontend/origami/default/js/menu.js
+++ stoptransit/skin/frontend/origami/default/js/menu.js
@@ -1,25 +1,25 @@
-jQuery.fn.mainMenu = function(){
-    jQuery(this).each(function(){
+jQuery.fn.mainMenu = function () {
+    jQuery(this).each(function () {
         var mainItem = jQuery(this),
-            mainWidth = mainItem.width() + parseInt(mainItem.css('padding-left'))* 2,
+            mainWidth = mainItem.width() + parseInt(mainItem.css('padding-left')) * 2,
             mainItems = mainItem.find('li.level-top'),
             subItems = mainItems.find('div.sub-menu');
 
-        subItems.each(function(){
+        subItems.each(function () {
             var el = jQuery(this),
                 elWidth = el.find('.sub-menu-col').length * 150;
-            if(!el.hasClass('complex-menu')){
+            if (!el.hasClass('complex-menu')) {
                 el.css('width', elWidth);
                 elWidth = elWidth + 40;
                 var delta = (mainItem.offset().left + mainWidth) - (el.offset().left + elWidth);
-                if(delta < 0){
-                    el.css('left',delta-20);
+                if (delta < 0) {
+                    el.css('left', (delta - 20));
                 }
-            }else{
+            } else {
                 var complexCols = el.find('.complex-submenu'),
                     complexNav = el.find('.complex-nav li');
-                complexNav.each(function(){
-                    jQuery(this).bind('mouseenter',function(){
+                complexNav.each(function () {
+                    jQuery(this).bind('mouseenter', function () {
                         complexCols.hide();
                         complexNav.removeClass('active');
                         complexCols.eq(jQuery(this).index()).show();
@@ -27,18 +27,17 @@ jQuery.fn.mainMenu = function(){
                     });
                 });
                 var delta = (mainItem.offset().left + mainWidth) - (el.offset().left + 600);
-                if(delta < 0){
-                    el.css('left',delta-70);
+                if (delta < 0) {
+                    el.css('left', (delta - 70));
                 }
-
             }
         })
 
-        mainItems.on('mouseenter',function(){
+        mainItems.on('mouseenter', function () {
             var el = jQuery(this).addClass('hovered');
             var complexCols = el.find('.complex-submenu'),
                 complexNav = el.find('.complex-nav li');
-            if(complexCols.length && complexNav.length){
+            if (complexCols.length && complexNav.length) {
                 complexCols.hide();
                 complexNav.removeClass('active');
                 complexCols.eq(0).show();
@@ -46,12 +45,15 @@ jQuery.fn.mainMenu = function(){
             }
 
         })
-            .on('mouseleave',function(){
+            .on('mouseleave', function () {
                 jQuery(this).removeClass('hovered');
             });
     })
 };
 
-jQuery(document).ready(function(){
+$(document).observe('dom:loaded', function () {
+    jQuery('#top_menu_block').mainMenu();
+});
+jQuery(window).load(function () {
     jQuery('#top_menu_block').mainMenu();
 });
